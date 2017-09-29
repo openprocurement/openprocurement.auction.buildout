61888322198d381a309c0a8c3cd3518b230ed962
godsdog <an.andrusyk@gmail.com>
2017-09-29 19:20:17 +0300

    Fix test.py

--- test.py
+++ test.py
@@ -1,30 +1,52 @@
-#!bin/python
+#!bin/python_interpreter
+
+# HOW TO USE:
+# run the command:
+# ./test.py simple planning && ./test.py simple run
+# or
+# ./test.py multilot planning && ./test.py multilot run
+# or
+# ./test.py esco planning && ./test.py esco run
+
 import os
 import os.path
 import os.path
 import datetime
 import json
-import sys
 import argparse
 import contextlib
 import tempfile
 from dateutil.tz import tzlocal
 from subprocess import check_output
+from datetime import datetime, timedelta
+
+
+PAUSE_SECONDS = timedelta(seconds=120)
 
 
 PWD = os.path.dirname(os.path.realpath(__file__))
 CWD = os.getcwd()
-TENDER = os.path.join(PWD, 'src/openprocurement.auction.esco/openprocurement/auction/esco/tests/functional/data/tender_esco.json')
-WORKER = 'auction_esco'
-CONFIG = 'auction_worker_esco.yaml'
+TENDER = {'simple': os.path.join(PWD, 'src/openprocurement.auction.worker/openprocurement/auction/worker/tests/functional/data/tender_simple.json'),
+          'multilot': os.path.join(PWD, 'src/openprocurement.auction.worker/openprocurement/auction/worker/tests/functional/data/tender_multilot.json'),
+          'esco': 'src/openprocurement.auction.esco/openprocurement/auction/esco/tests/functional/data/tender_esco.json'}
+
+WORKER = {'simple': 'auction_worker',
+          'multilot': 'auction_worker',
+          'esco': 'auction_esco'}
 
 
 @contextlib.contextmanager
 def update_auctionPeriod(path, auction_type):
     with open(path) as file:
         data = json.loads(file.read())
-    new_start_time = (datetime.datetime.now(tzlocal()) + datetime.timedelta(seconds=20)).isoformat()
-    data['data']['auctionPeriod']['startDate'] = new_start_time
+    new_start_time = (datetime.now(tzlocal()) + PAUSE_SECONDS).isoformat()
+
+    if auction_type == 'simple':
+        data['data']['auctionPeriod']['startDate'] = new_start_time
+    elif auction_type == 'multilot':
+        for lot in data['data']['lots']:
+            lot['auctionPeriod']['startDate'] = new_start_time
+
     with tempfile.NamedTemporaryFile(delete=False) as auction_file:
         json.dump(data, auction_file)
         auction_file.seek(0)
@@ -32,27 +54,31 @@ def update_auctionPeriod(path, auction_type):
     auction_file.close()
 
 
-def planning(tender_file_path, auction_id):
-    with update_auctionPeriod(tender_file_path, auction_type='simple') as auction_file:
+def planning(tender_file_path, worker, auction_id):
+    with update_auctionPeriod(tender_file_path,
+                              auction_type='simple') as auction_file:
         os.system('{0}/bin/{1} planning {2}'
-                     ' {0}/etc/{3} --planning_procerude partial_db --auction_info {4}'.format(CWD, WORKER,
-                                                                                                  auction_id, CONFIG,
-                                                                                                  auction_file))
+                  ' {0}/etc/auction_worker_defaults.yaml --planning_procerude partial_db --auction_info {3}'.format(
+            CWD, worker, auction_id, auction_file))
     os.system('sleep 3')
 
-def run(tender_file_path, auction_id):
-    with update_auctionPeriod(tender_file_path, auction_type='simple') as auction_file:
+
+def run(tender_file_path, worker, auction_id):
+    with update_auctionPeriod(tender_file_path,
+                              auction_type='simple') as auction_file:
         check_output('{0}/bin/{1} run {2}'
-                     ' {0}/etc/{3} --planning_procerude partial_db --auction_info {4}'.format(CWD, WORKER,
-                                                                                                  auction_id, CONFIG,
-                                                                                                  auction_file).split())
+                     ' {0}/etc/auction_worker_defaults.yaml --planning_procerude partial_db --auction_info {3}'.format(
+            CWD, worker, auction_id, auction_file).split())
     os.system('sleep 3')
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('type', type=str)
+    parser.add_argument('auction_type', type=str)
+    parser.add_argument('action_type', type=str)
 
     args = parser.parse_args()
+
     actions = globals()
-    if args.type in actions:
-        actions.get(args.type)(TENDER, "11111111111111111111111111111111")
+    if args.action_type in actions:
+        actions.get(args.action_type)(TENDER[args.auction_type], WORKER[args.auction_type], "11111111111111111111111111111111")
